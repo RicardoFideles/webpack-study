168a022b6f6ac5fcac74bf865d3883c42ed333a7
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-08-01 11:38:09 -0300

    few test with webpack

---
+++ ex/index.js
@@ -0,0 +1,32 @@
+//const {info} = require('./logger')
+//info('console.looo');
+//import Pessoa from './pessoa';
+
+//const pessoa = new Pessoa('Ricardo');
+//console.log(pessoa.toString());
+
+/*
+const produto = {
+    nome : 'Caneca',
+    preco : 1.90,
+    desconto : 0.05
+}
+
+function clone (obj) {
+    return { ...obj } //spread para cloncar objetos.
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta 2';
+
+console.log(produto, novoProduto);
+*/
+import './style.css'
+import 'react'
+
+export default props => (
+    <h1>Olá</h1>
+)
+
+console.log('Funcionou!')
+
